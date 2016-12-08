283a7fc2d62c0a900c7a82de4afcbb8baeb47738
Guoxin <suiguoxin@gmail.com>
2016-12-08 11:52:31 +0100

    la partie GUI
    interface du damier

---
+++ GUI.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JeuGo;
+
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * classe pour le frame
+ * @author Guoxin
+ */
+
+public class GUI extends Frame {
+
+    ChessPad chesspad = new ChessPad();
+
+    GUI() {
+        setVisible(true);
+        setLayout(null);
+        Label label = new Label("JeuGo", Label.CENTER);
+        add(label);
+        label.setBounds(70, 55, 440, 26);
+        label.setBackground(Color.yellow);
+        add(chesspad);
+        chesspad.setBounds(70, 90, 440, 440);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        }
+        );
+        pack();
+        setSize(600, 550);
+    }
+
+    public static void main(String args[]) {
+        GUI gui = new GUI();
+    }
+}
