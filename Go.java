7493103fb1db9c26dfa25549e025d9ceeac5dd7e
Rouillon <fabien.rouillon@eleves.ec-nantes>
2016-12-08 10:04:10 +0100

    test

---
+++ Go.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package go.JeuGo;
+
+/**
+ *
+ * @author fabienrouillon
+ */
+public class Go {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //test
+        // TODO code application logic here
+    }
+    
+}
