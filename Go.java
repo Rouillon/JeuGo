2ed4b7b5eee876e62ac5e2e5e470f07965d1376d
remi-rosenthal <remi.rosenthal@eleves.ec-nantes.fr>
2016-12-08 10:38:10 +0100

    clear

--- Go.java
+++ Go.java
@@ -1,24 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package JeuGo;
 
-/**
- *
- * @author fabienrouillon
- */
 public class Go {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        
-        int c=3;
-        // TODO code application logic here
-        // ca test
     }
     
 }
