f5ccd42fa2fe7ffa6451ae52e355701342f26bd6
remi-rosenthal <remi.rosenthal@eleves.ec-nantes.fr>
2016-12-08 11:41:35 +0100

    pions

--- Jeu.java
+++ Jeu.java
@@ -5,6 +5,8 @@
  */
 package JeuGo;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author fabienrouillon
@@ -19,6 +21,26 @@ public class Jeu {
         for(int i=0; i<16; i++){
             for(int j=0; j<16; j++){
                 this.plateau[i][j]=0;
+            }
+        }
+    }
+    
+    public ArrayList<int[]> adjacent(int[] point){
+        
+    }
+    public ArrayList<ArrayList> groupes(){
+        ArrayList<ArrayList> groupe = new ArrayList();
+        ArrayList<int[]> marques = new ArrayList();
+        int[] point = new int[2];
+        for(int i=0; i<16; i++){
+            for(int j=0; j<16; j++){
+                if(this.plateau[i][j]!=0){
+                    point[0] = i;
+                    point[1] = j;
+                    marques.add(point);
+                }
+                
+                
             }
         }
     }
