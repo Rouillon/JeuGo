cb16b24c8771b007b68f061cd459fef60289634d
Rouillon <fabien.rouillon@eleves.ec-nantes>
2016-12-12 10:39:05 +0100

    Motifs jeu+ classes tests

--- Jeu.java
+++ Jeu.java
@@ -21,17 +21,12 @@ public class Jeu {
     public static String s; //pour demander le joueur est-ce qu'il veut passer ce manche. ça doit etre "oui" ou "non"
     private int captureN;
     private int captureB;
-    int[][] plateau = new int[16][16];
     ArrayList<Pion> listeTestes;
     ArrayList<ArrayList<Pion>> listeGroupes;
 
 
     public Jeu() {
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 16; j++) {
-                this.plateau[i][j] = 0;
-            }
-        }
+        
     }
 
     public static boolean poserPierreNoir(int[][] plateau, int x, int y) {
@@ -49,6 +44,7 @@ public class Jeu {
 
             if (s.equals("oui")) {
                 passerNoir = true;
+                continuer = false;
             } else {
                 {
                     if (plateau[x][y] == 0 && x < 16 && x >= 0 && y < 16 && y >= 0) {
@@ -88,7 +84,7 @@ public class Jeu {
                             plateau[x][y] = -1; //-1 est pout les pierres blancs
                             
                         }
-                    continuer=false;  
+                        continuer=false;  
                     } else {
                         System.out.println("Vous pouvez pas le poser ici!");
                         continuer = true;
@@ -218,48 +214,49 @@ public class Jeu {
         return nbBlanc;
     }
 
-    public ArrayList<Pion> adjacents(Pion p){
+    public ArrayList<Pion> adjacents(int[][] plateau, Pion p){
         int px = p.getX();
         int py = p.getY();
         int couleur = p.getCouleur();
         ArrayList<Pion> adjacents = new ArrayList();
-        if (this.plateau[px-1][py]==couleur){
+        if (plateau[px-1][py]==couleur){
             adjacents.add(new Pion(px-1,py));
         }
-        if (this.plateau[px+1][py]==couleur){
+        if (plateau[px+1][py]==couleur){
             adjacents.add(new Pion(px+1,py));
         }
-        if (this.plateau[px][py-1]==couleur){
+        if (plateau[px][py-1]==couleur){
             adjacents.add(new Pion(px,py-1));
         }
-        if (this.plateau[px][py+1]==couleur){
+        if (plateau[px][py+1]==couleur){
             adjacents.add(new Pion(px,py+1));
         }
         return adjacents;
     }
-    public ArrayList<Pion> ajouterAdjacent(Pion p, ArrayList<Pion> groupe){
+    
+    public ArrayList<Pion> ajouterAdjacent(int[][] plateau,Pion p, ArrayList<Pion> groupe){
         groupe.add(p);
         this.listeTestes.add(p);
-        ArrayList<Pion> adjacents = this.adjacents(p);
+        ArrayList<Pion> adjacents = adjacents(plateau, p);
         for(Pion adja : adjacents){
             if(!this.listeTestes.contains(adja)){
-                this.ajouterAdjacent(adja, groupe);
+                this.ajouterAdjacent(plateau,adja, groupe);
             }
         }
         return groupe;
     }
     
-    public void detectionGroupes(){
+    public void detectionGroupes(int[][] plateau){
         this.listeTestes.clear();
         this.listeGroupes.clear();
         for(int i=0; i<16; i++){
             for(int j=0; j<16; j++){
                 //blanc 
-                if(this.plateau[i][j]!=0){
+                if(plateau[i][j]!=0){
                     Pion p = new Pion(i,j);
                     if(!this.listeTestes.contains(p)){
                         ArrayList<Pion> nouveau = new ArrayList<>();
-                        this.listeGroupes.add(this.ajouterAdjacent(p,nouveau));
+                        this.listeGroupes.add(this.ajouterAdjacent(plateau,p,nouveau));
                     }
                 }
             }
@@ -271,14 +268,14 @@ public class Jeu {
         ArrayList<Pion> adj;
         for (ArrayList<Pion> groupe:this.listeGroupes){
             for (Pion p: groupe){
-                adj = this.adjacents(p);
+                adj = adjacents(plateau,p);
                 if (adj.size()<4){ // TODO ou sur le bord
                     capture=false;
                 }
             }
             if (capture) {
                     for (Pion p:groupe) {
-                    this.plateau[p.getX()][p.getY()]=0;
+                    plateau[p.getX()][p.getY()]=0;
                 }
                 }
         }
